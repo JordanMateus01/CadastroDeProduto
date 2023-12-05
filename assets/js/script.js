7c33917a2d3d055dcb7b153d34998a2fcac8885d
Jordan Mateus <jordanmateus262@gmail.com>
2023-12-05 08:48:49 -0300

    ocultando e exibindo tabela dinamicamente

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,8 +3,11 @@ function cadastrarProduto(codigoProduto, nomeProduto, descricaoProduto, valorPro
     var nomeProduto = document.getElementById("nomProd").value;
     var descricaoProduto = document.getElementById("descProd").value;
     var valorProduto = document.getElementById("valProd").value
+    var tabela = document.getElementById("tabelaDeProdutos");
 
     if(codigoProduto.length > 0 && nomeProduto.length > 0 && descricaoProduto.length > 0 && valorProduto.length > 0){
+        tabela.style.display = "table";
+
         var codigo = document.getElementById("codigo").innerHTML = codigoProduto;
         var nome = document.getElementById("nome").innerHTML = nomeProduto;
         var descricao = document.getElementById("descricao").innerHTML = descricaoProduto;
