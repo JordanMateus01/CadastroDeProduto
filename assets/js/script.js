bfc29dd8bb40a5ed38db4f38bab1406c2a5620b2
Jordan Mateus <jordanmateus262@gmail.com>
2023-12-26 20:39:45 -0300

    Atualizando nomeclatura dos parametros

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,4 @@
-function cadastrarProduto(codProd, nomProd, descProd, valProd){
+function cadastrarProduto(codigoProduto, nomeProduto, descricaoProduto, valorProduto){
     var tabela = document.getElementById("tabelaDeProdutos");
     var qtdlinhas =  tabela.rows.length;
     var line = tabela.insertRow(qtdlinhas);
@@ -6,10 +6,10 @@ function cadastrarProduto(codProd, nomProd, descProd, valProd){
     if(codProd.length > 0 && nomProd.length > 0 && descProd.length > 0 && valProd.length > 0){
         tabela.style.display = "table";
 
-        var codigo = line.insertCell(0).innerHTML = codProd;
-        var nome = line.insertCell(1).innerHTML = nomProd;
-        var descricao = line.insertCell(2).innerHTML = descProd;
-        var valor = line.insertCell(3).innerHTML = "R$ " + valProd;
+        var codigo = line.insertCell(0).innerHTML = codigoProduto;
+        var nome = line.insertCell(1).innerHTML = nomeProduto;
+        var descricao = line.insertCell(2).innerHTML = descricaoProduto;
+        var valor = line.insertCell(3).innerHTML = "R$ " + valorProduto;
     } else {
         alert("Preencha todos o campos.")
     }
