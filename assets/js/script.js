d33f471beceb197759e62725ffd8e28023bac2e0
Jordan Mateus <jordanmateus262@gmail.com>
2023-12-26 19:55:00 -0300

    Excluindo variáveis referentes ao botão editar

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,11 +2,9 @@ function cadastrarProduto(codProd, nomProd, descProd, valProd){
     var tabela = document.getElementById("tabelaDeProdutos");
     var qtdlinhas =  tabela.rows.length;
     var line = tabela.insertRow(qtdlinhas);
-    var botaoEdicao = document.getElementById("btnEdicao");
 
     if(codProd.length > 0 && nomProd.length > 0 && descProd.length > 0 && valProd.length > 0){
         tabela.style.display = "table";
-        botaoEdicao.style.display = "block";
 
         var codigo = line.insertCell(0).innerHTML = codProd;
         var nome = line.insertCell(1).innerHTML = nomProd;
@@ -16,3 +14,4 @@ function cadastrarProduto(codProd, nomProd, descProd, valProd){
         alert("Preencha todos o campos.")
     }
 }
+
